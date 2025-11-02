27936d6b66ea37e53f70d674097c750a71e65421
vldmirr <tsutsura18@gmail.com>
2025-11-02 23:16:49 +0300

    Pascal's Triangle

--- Lissts.py
+++ Lissts.py
@@ -11,10 +11,36 @@ class Solution:
             digits.append(int(num[i]))
 
         return digits
+    
+    #Pascal's Triangle
+    def generate(self, numRows: int) -> list[list[int]]:
+        triangle = []
+        
+        for i in range(numRows):
+            
+#             [1]
+#           [1, 1]
+#          [1, 1, 1]
+#         [1, 1, 1, 1]
+#       [1, 1, 1, 1, 1]
+#        .............
+            row = [1] * (i + 1)
+            print(row)
+            
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            
+            triangle.append(row)
+        
+        return triangle
 
 d=[4,3,2,1]
 #d=[9,9]
 #d=[9]
 
+#1
+#2
+
 o=Solution()
 print(o.plusOne([4, 3, 2, 1]))
+print(o.generate(5))
